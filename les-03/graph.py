f1c08ec6c78a13dd6bb0ad556e63f103fe489582
Your Name <your.email@example.com>
2025-08-03 04:39:23 +0500

    Add graph.py to implement a chatbot using LangChain and TavilySearch, including state management and tool integration.

---
+++ les-03/graph.py
@@ -0,0 +1,41 @@
+from typing import Annotated
+
+from langchain.chat_models import init_chat_model
+from langchain_tavily import TavilySearch
+from typing_extensions import TypedDict
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph import StateGraph
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = init_chat_model("anthropic:claude-3-5-sonnet-latest", temperature=0.7)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+# add tools to the LLM
+tool = TavilySearch(max_results=2)
+tools = [tool]
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.set_entry_point("chatbot")
+memory = InMemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
