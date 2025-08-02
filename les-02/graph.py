2f0c209016d86e3e8e13556f650ecba1e746e6ad
Your Name <your.email@example.com>
2025-08-03 01:33:46 +0500

    Add LangSmith API endpoint to README and include egg-info files for package metadata, dependencies, and sources.

---
+++ les-02/graph.py
@@ -0,0 +1,44 @@
+from typing import Annotated
+from langchain_tavily import TavilySearch
+from typing_extensions import TypedDict
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+from langgraph.prebuilt import ToolNode, tools_condition
+from dotenv import load_dotenv
+
+# load environment variables
+load_dotenv()
+
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+
+graph_builder = StateGraph(State)
+
+tool = TavilySearch(max_results=2)
+tools = [tool]
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+graph_builder.add_node("chatbot", chatbot)
+
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition,
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+graph_builder.add_edge(START, "chatbot")
+graph_builder.add_edge("chatbot", END)
+graph = graph_builder.compile()
+
+
+
+# pip install -U langchain-tavily
